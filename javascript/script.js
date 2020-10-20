ec968927e07895adc29114a0bc143a6f17019e15
shehab-ahmad <tt3lm464@gmail.com>
2020-10-20 21:50:56 +0300

    done 20-10-2020

---
+++ javascript/script.js
@@ -0,0 +1,154 @@
+          
+                // var audio = document.getElementById("audioMusic");
+                // function A(){
+                //         audio.play();
+                // }
+                
+      
+  // أ sound effect
+  var AA = document.getElementById("AA");
+        function AAA(){
+                AA.play();
+        }
+         // ب sound effect
+        var BB = document.getElementById("BB");
+        function BBB(){
+            BB.play();
+        }
+        
+        // ت sound effect
+        var CC = document.getElementById("CC");
+        function CCC(){
+                CC.play();
+        }
+         // ث sound effect
+        var DD = document.getElementById("DD");
+        function DDD(){
+            DD.play();
+        }
+         // ج sound effect
+        var EE = document.getElementById("EE");
+        function EEE(){
+            EE.play();
+        }
+        // ح sound effect
+        var FF = document.getElementById("FF");
+        function FFF(){
+            FF.play();
+        }
+           // خ sound effect
+        var GG = document.getElementById("GG");
+        function GGG(){
+            GG.play();
+        }
+           // د sound effect
+           var HH = document.getElementById("HH");
+        function HHH(){
+            HH.play();
+        }
+           // ذ sound effect
+           var II = document.getElementById("II");
+        function III(){
+            II.play();
+        }
+           // ر sound effect
+           var JJ = document.getElementById("JJ");
+        function JJJ(){
+            JJ.play();
+        }
+           // ز sound effect
+           var KK = document.getElementById("KK");
+        function KKK(){
+            KK.play();
+        }
+
+           // س sound effect
+           var MM = document.getElementById("MM");
+        function MMM(){
+            MM.play();
+        }
+           // ش sound effect
+           var NN = document.getElementById("NN");
+        function NNN(){
+            NN.play();
+        }
+           // ص sound effect
+           var OO = document.getElementById("OO");
+        function OOO(){
+            OO.play();
+        }
+           // ض sound effect
+           var PP = document.getElementById("PP");
+        function PPP(){
+          PP.play()
+        }
+           // ط sound effect
+           var QQ = document.getElementById("QQ");
+        function QQQ(){
+            QQ.play();
+        }
+           // ظ sound effect
+           var RR = document.getElementById("RR");
+        function RRR (){
+          RR.play()
+        }
+         // ع sound effect
+         var SS = document.getElementById("SS");
+        function SSS(){
+            SS.play();
+        }
+           // غ sound effect
+           var TT = document.getElementById("TT");
+        function TTT (){
+          TT.play()
+        }
+           // ف sound effect
+        var UU = document.getElementById("UU");
+        function UUU(){
+            UU.play();
+        }
+           // ق sound effect
+           var VV = document.getElementById("VV");
+        function VVV (){
+          VV.play()
+        }
+             // ك sound effect
+             var WW = document.getElementById("WW");
+        function WWW(){
+            WW.play();
+        }
+           // ل sound effect
+           var XX = document.getElementById("XX");
+        function XXX (){
+          XX.play()
+        }
+            // م sound effect
+            var YY = document.getElementById("YY");
+        function YYY(){
+            YY.play();
+        }
+           // ن sound effect
+           var ab = document.getElementById("ab");
+        function abb (){
+          ab.play()
+        }
+
+          // ه sound effect
+          var ac = document.getElementById("ac");
+        function acc (){
+          ac.play()
+        }
+            // و sound effect
+            var ad = document.getElementById("ad");
+        function add(){
+            ad.play();
+        }
+           // ي sound effect
+           var ae = document.getElementById("ae");
+        function aee (){
+          ae.play()
+        }
+        
+
+                   
+               
---
+++ javascript/script1.js
@@ -0,0 +1,62 @@
+          
+                // var audio = document.getElementById("audioMusic");
+                // function A(){
+                //         audio.play();
+                // }
+                
+      
+        // 1 sound effect
+        var AA = document.getElementById("AA");
+        function AAA(){
+                AA.play();
+        }
+         // 2 sound effect
+        var BB = document.getElementById("BB");
+        function BBB(){
+            BB.play();
+        }
+        
+        // 3 sound effect
+        var CC = document.getElementById("CC");
+        function CCC(){
+                CC.play();
+        }
+         // 4 sound effect
+        var DD = document.getElementById("DD");
+        function DDD(){
+            DD.play();
+        }
+         // 5 sound effect
+        var EE = document.getElementById("EE");
+        function EEE(){
+            EE.play();
+        }
+        // 6 sound effect
+        var FF = document.getElementById("FF");
+        function FFF(){
+            FF.play();
+        }
+           // 7 sound effect
+        var GG = document.getElementById("GG");
+        function GGG(){
+            GG.play();
+        }
+           // 8 sound effect
+           var HH = document.getElementById("HH");
+        function HHH(){
+            HH.play();
+        }
+           // 9 sound effect
+           var II = document.getElementById("II");
+        function III(){
+            II.play();
+        }
+           // 10 sound effect
+           var JJ = document.getElementById("JJ");
+        function JJJ(){
+            JJ.play();
+        }
+         
+
+                   
+               
